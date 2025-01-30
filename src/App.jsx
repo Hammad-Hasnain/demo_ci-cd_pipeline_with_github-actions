babaeff7ea32dcf7305d90f9a7918faf1ddf3900
Hammad-Hasnain <hammadhasnain81@gmail.com>
2025-01-30 16:55:25 +0500

    react demo project for implementing ci-cd pipeline using github actions

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h1 style={{ textAlign: "center", fontFamily: "sans-serif" }}>
+        CI/CD Pipeline with github actions
+      </h1>
+    </div>
+  );
+};
+
+export default App;
