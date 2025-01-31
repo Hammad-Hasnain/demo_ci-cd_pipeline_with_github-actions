badf975c1688d17b8d4bf282ea3c6a56d8760994
Hammad-Hasnain <hammadhasnain81@gmail.com>
2025-01-31 11:32:45 +0500

    base path added

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/demo_ci-cd_pipeline_with_github-actions/", // tells vite from where the subdirectory will be served
+});
