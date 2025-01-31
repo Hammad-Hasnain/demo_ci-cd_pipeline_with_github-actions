50f0cd0a652582663abc2592501d651a5de90140
Hammad-Hasnain <hammadhasnain81@gmail.com>
2025-01-31 11:59:50 +0500

    "Congrats line" added to cross check check CI-CD Pipeline

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ const App = () => {
       <h1 style={{ textAlign: "center", fontFamily: "sans-serif" }}>
         CI/CD Pipeline with github actions
       </h1>
+      <h2>WoHoo! Successfully CI/CD Pipeline implemented and deployed </h2>
     </div>
   );
 };
